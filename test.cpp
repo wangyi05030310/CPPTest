d3e740d197bfcc1dfd19569e1df3c86628994c0e
wangyi05030310 <wangyi05030310@gmail.com>
2012-01-13 02:01:29 +0800

    configure vim and have a test with it.

---
+++ test.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	int b = 3;
+
+	if (a < b)
+	{
+		cout << "hello" << endl;
+	}
+	else
+	{
+		cout << "world" << endl;
+	}
+
+	return 0;
+}
